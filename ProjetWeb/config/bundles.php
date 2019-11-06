8ebed505f5f59ed1e0ad3cdeb1bc80032e80642d
Slash-12 <baptiste.moench@viacesi.fr>
2019-11-06 10:00:21 +0100

    UwU

--- ProjetWeb/config/bundles.php
+++ ProjetWeb/config/bundles.php
@@ -16,5 +16,4 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
-    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
