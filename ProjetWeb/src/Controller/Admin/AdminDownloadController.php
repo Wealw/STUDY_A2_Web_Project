e47b3d819c4fc45a3d82d148f559078493c665a4
hugodegrossi <hugodegrossi@gmail.com>
2019-11-15 09:48:47 +0100

    ZIP encore une fois

--- ProjetWeb/src/Controller/Admin/AdminDownloadController.php
+++ ProjetWeb/src/Controller/Admin/AdminDownloadController.php
@@ -13,10 +13,10 @@ use ZipArchive;
 class AdminDownloadController extends AbstractController
 {
     /**
-     * @Route("/download/images", name="download.images")
+     * @Route("/download/products", name="download.products")
      * @return BinaryFileResponse
      */
-    public function download()
+    public function downloadProd()
     {
         $path = explode("\src", __DIR__ );
         $imagesProds = scandir($path[0] . '\public\assets\images\products');
@@ -33,5 +33,52 @@ class AdminDownloadController extends AbstractController
         }
         $zip->close();
         return $this->file($path[0] . '\public\assets\downloadimages.zip');
+    }
+
+    /**
+     * @Route("/download/events", name="download.events")
+     * @return BinaryFileResponse
+     */
+    public function downloadEv()
+    {
+        $path = explode("\src", __DIR__ );
+        $imagesEvs = scandir($path[0] . '\public\assets\images\events');
+        $imgPath = $path[0] . '\public\assets\images\events';
+        $zipPath = $path[0] . '\public\assets\downloadevents.zip';
+        $zip = new ZipArchive();
+        $zip->open($zipPath, ZIPARCHIVE::CREATE | ZIPARCHIVE::OVERWRITE);
+        foreach ($imagesEvs as $key=>$imagesEv)
+        {
+            if($key > 1)
+            {
+                $zip->addFile($zipPath, $imagesEv);
+            }
+        }
+        $zip->close();
+        return $this->file($path[0] . '\public\assets\downloadevents.zip');
+    }
+
+
+    /**
+     * @Route("/download/pictures", name="download.pictures")
+     * @return BinaryFileResponse
+     */
+    public function downloadPic()
+    {
+        $path = explode("\src", __DIR__ );
+        $imagesPics = scandir($path[0] . '\public\assets\images\pictures');
+        $imgPath = $path[0] . '\public\assets\images\pictures';
+        $zipPath = $path[0] . '\public\assets\downloadpictures.zip';
+        $zip = new ZipArchive();
+        $zip->open($zipPath, ZIPARCHIVE::CREATE | ZIPARCHIVE::OVERWRITE);
+        foreach ($imagesPics as $key=>$imagesPic)
+        {
+            if($key > 1)
+            {
+                $zip->addFile($zipPath, $imagesPic);
+            }
+        }
+        $zip->close();
+        return $this->file($path[0] . '\public\assets\downloadpictures.zip');
     }
 }
